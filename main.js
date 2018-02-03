574fbea5f5751257335abc469b0eb1b8e9cda9ac
Unknown <segler_alex@web.de>
2018-02-03 15:19:02 +0100

    show all servers from dns

--- main.js
+++ main.js
@@ -2,16 +2,18 @@ var app = new Vue({
     el: '#app',
     data: {
       items: [
-        { message: 'Foo' },
-        { message: 'Bar' }
       ]
     }
   });
-app.items[0].message = "NUUU";
+
+function StringSort(str1,str2){
+    return str1 < str2 ? -1 : str1 > str2;
+}
 
 axios.get('https://api.radio-browser.info/json/servers')
 .then(function (response) {
-    console.log(response);
+    app.items = response.data;
+    app.items.sort(function(a,b){return StringSort(a.name,b.name);});
 })
 .catch(function (error) {
     console.log(error);
