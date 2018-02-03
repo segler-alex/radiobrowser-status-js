092d427bcc7afd17b9d43fdee0735296f77d2d4d
Unknown <segler_alex@web.de>
2018-02-03 15:48:47 +0100

    check all servers live

--- main.js
+++ main.js
@@ -12,8 +12,32 @@ function StringSort(str1,str2){
 
 axios.get('https://api.radio-browser.info/json/servers')
 .then(function (response) {
-    app.items = response.data;
+    var names = response.data.map(function(item){
+        return item.name;
+    });
+    var filtered = [];
+    for (var i=0;i<names.length;i++){
+        if (filtered.indexOf(names[i]) < 0) {
+            filtered.push(names[i]);
+        }
+    }
+
+    app.items = filtered.map(function(name){
+        return {name, status: 'WAITING', started: Date.now()};
+    });
     app.items.sort(function(a,b){return StringSort(a.name,b.name);});
+
+    var list = app.items.map(function(item){
+        return axios.get('https://'+item.name+'/json/servers')
+        .then(function(result){
+            item.status = 'OK';
+            item.time = (Date.now() - item.started)/1000;
+        }).catch(function(err){
+            item.status = 'NOK';
+            item.time = (Date.now() - item.started)/1000;
+        });
+    });
+    return Promise.all(list);
 })
 .catch(function (error) {
     console.log(error);
